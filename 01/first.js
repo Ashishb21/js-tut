c7429d2d31d36460ad6600a16e377eeaf6f69d46
ashishb21 <ashishb21@gmail.com>
2025-08-22 01:59:03 +0530

    added files

---
+++ 01/first.js
@@ -0,0 +1,8 @@
+console.log("hi This is my first")
+const account=0
+var accountPasword="1233" // scope changes
+let accountemail="dddd" // no scope changes 
+accountcity="kndnd"
+let accountstate // best used 
+
+console.table([account,accountPasword,accountemail,accountcity,accountstate])
---
+++ 01/second.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3)// using node js not browser
+
+let name="hitesh" //string
+let age=18
+let isloggedin=true
+
+//number
+//bigint
+//boolean
+//null  =>standalone value null is empty not zero
+//undefined ==> not defined any value
+//symbol ==>unique
+//object 
+
+console.log(typeof age);
+console.log(typeof udefined)
+console.log(typeof null)
+
