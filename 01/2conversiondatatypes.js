6dc943bfeb693d58f26425855ec57ce5fc9e0d72
ashishb21 <ashishb21@gmail.com>
2025-08-22 11:56:09 +0530

    added conversions

---
+++ 01/2conversiondatatypes.js
@@ -0,0 +1,47 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3)// using node js not browser
+
+let name1=null //string
+let age=18
+let isloggedin=true
+
+//number
+//bigint
+//boolean
+//null  =>standalone value null is empty not zero
+//undefined ==> not defined any value
+//symbol ==>unique
+//object 
+
+//console.log(typeof age);
+//console.log(typeof udefined)
+//console.log(typeof null)
+
+let Numbervalue=Number(name1)
+
+console.log(Numbervalue)
+
+//"33" ==>33
+//"33f ff" ==>NaN
+//"true" ==>1
+//null ==> 0
+//undefined ==> NaN
+
+let booleannumber="sss"
+
+//console.log(Boolean(booleannumber))
+
+//Boolean 
+
+//1==> true
+//0==>false
+//""==>false
+//"ddd"=>true
+
+
+// String 
+
+let somenumber=33
+
+console.log(typeof String(somenumber))
